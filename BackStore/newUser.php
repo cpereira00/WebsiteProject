6444d70321dff0923f09669e5f5d8c59971ffdc3
James <james@elman.org>
2020-08-16 13:18:31 -0400

    php

--- BackStore/newUser.php
+++ BackStore/newUser.php
@@ -36,24 +36,20 @@
                   <div class="col-sm-6">
                       <h2>New User</h2>
                       <br/>
-                    <form>
+                      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
                         <div class="form-group">
                           <label for="firstName">First Name:</label>
-                          <input type="text" class="form-control" id="firstName">
+                          <input type="text" class="form-control" id="firstName" name="fName">
                         </div>
                         <div class="form-group">
                             <label for="lastName">Last Name:</label>
-                            <input type="text" class="form-control" id="lastName">
+                            <input type="text" class="form-control" id="lastName" name="lName">
                         </div>
                         <div class="form-group">
                             <label for="email">Email:</label>
-                            <input type="email" class="form-control" id="email">
+                            <input type="email" class="form-control" id="email" name="email">
                         </div>
-                        <div class="form-group">
-                            <label for="phone">Phone Number:</label>
-                            <input type="tel" class="form-control" id="phone">
-                        </div>
-                    
+                        
                         <button type="submit" class="btn btn-default" >Submit</button>
                       </form>
                     </div>
@@ -62,5 +58,61 @@
           </body>
       </div>
 
+      <?php
+// define variables and set to empty values
+$nameErr = $lNameErr = $emailErr ="";
+$fName = $lName = $email ="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["fName"])) {
+    $fNameErr = "Name is required";
+  } else {
+    $fName = test_input($_POST["fName"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$fName)) {
+      $fNameErr = "Only letters and white space allowed";
+    }
+  }
+  if (empty($_POST["lName"])) {
+    $lNameErr = "Name is required";
+  } else {
+    $lName = test_input($_POST["lName"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$lName)) {
+      $lNameErr = "Only letters and white space allowed";
+    }
+  }
   
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+      <?php
+
+      $user = array($fName,$lName,$email);
+      
+      $fp = fopen('usersmtg.csv', 'a');
+      
+      
+          fputcsv($fp, $user);
+      
+      
+      fclose($fp);
+      ?>
 </html>
